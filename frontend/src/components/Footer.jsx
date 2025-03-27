18b8af58c1626cab270f793a93740d08d9ccd07f
A7isss <10.younis@gmail.com>
2025-03-27 23:04:51 +0300

    new 10

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,27 +8,6 @@ const Footer = () => {
   return (
     <div className='md:mx-10'>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-
-        <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>
-            {t('footer_description')} {/* Replace hardcoded text with translation key */}
-          </p>
-        </div>
-
-        <div>
-          <p className='text-xl font-medium mb-5'>{t('company')}</p> {/* Replace hardcoded text with translation key */}
-          <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>{t('home')}</li> {/* Replace hardcoded text with translation key */}
-            <li>{t('about_us')}</li> {/* Replace hardcoded text with translation key */}
-            <li>{t('delivery')}</li> {/* Replace hardcoded text with translation key */}
-            <li>{t('privacy_policy')}</li> {/* Replace hardcoded text with translation key */}
-          </ul>
-        </div>
-
-        <div>
-          <p className='text-xl font-medium mb-5'>{t('get_in_touch')}</p> {/* Replace hardcoded text with translation key */}
-          <ul className='flex flex-col gap-2 text-gray-600'>
             <li>920025092</li>
             <li>info@lahm.sa</li>
           </ul>
@@ -42,7 +21,7 @@ const Footer = () => {
           {t('copyright', { year: 2025 })} {/* Replace hardcoded text with translation key */}
         </p>
         <p className='py-5 text-xsm text-center'>
-          {('WWW.lahm.sa')} {/* Replace hardcoded text with translation key */}
+          {('www.lahm.sa')} {/* Replace hardcoded text with translation key */}
         </p>
       </div>
 
