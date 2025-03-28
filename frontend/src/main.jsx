2e4eb82ef7b446ba36fd797703791475a2d9524f
A7isss <10.younis@gmail.com>
2025-03-28 23:10:01 +0300

    new 22

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppContextProvider from './context/AppContext.jsx';
 import { CartContextProvider } from './context/CartContext.jsx'; // Import CartContextProvider by its named export
+import { VisitorProvider } from './context/VisitorContext.jsx'; // Import CartContextProvider by its named export
 import './i18n'; // Import i18n configuration
 import i18n from 'i18next'; // Import i18n instance
 
@@ -15,11 +16,13 @@ i18n.on('languageChanged', (lng) => {
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <AppContextProvider>
-            <CartContextProvider> {/* Make sure to use the correct provider name */}
-                <App />
-            </CartContextProvider>
-        </AppContextProvider>
-    </BrowserRouter>
+        <BrowserRouter>
+            <VisitorProvider> {/* Wrap everything */}
+                <AppContextProvider>
+                    <CartContextProvider> {/* Make sure to use the correct provider name */}
+                        <App />
+                    </CartContextProvider>
+                </AppContextProvider>
+            </VisitorProvider>
+        </BrowserRouter>
 );
