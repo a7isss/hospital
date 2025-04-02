9b94c3865eafc1e0aca9f5f1150a1fed8b711929
A7isss <10.younis@gmail.com>
2025-04-02 21:32:22 +0300

    login fix 37

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { CartContextProvider } from './context/CartContext.jsx'; // Import CartC
 import { VisitorProvider } from './context/VisitorContext.jsx'; // Import CartContextProvider by its named export
 import './i18n'; // Import i18n configuration
 import i18n from 'i18next'; // Import i18n instance
-
+import UserProvider from "./context/UserContext.jsx";
 // Dynamically set the `dir` attribute based on the selected language
 i18n.on('languageChanged', (lng) => {
     document.documentElement.setAttribute('dir', lng === 'ar' ? 'rtl' : 'ltr');
@@ -20,7 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <VisitorProvider> {/* Wrap everything */}
                 <AppContextProvider>
                     <CartContextProvider> {/* Make sure to use the correct provider name */}
+                        <UserProvider>
                         <App />
+                        </UserProvider>
                     </CartContextProvider>
                 </AppContextProvider>
             </VisitorProvider>
