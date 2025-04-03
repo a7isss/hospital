37e8c933f80ce1c5db2f2b0149d0ec5cc3fd880b
A7isss <10.younis@gmail.com>
2025-04-04 00:21:55 +0300

    login fix 49

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -94,7 +94,7 @@ const Nav = () => {
                                             to={item.path}
                                             className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-lg"
                                         >
-                                            {item.name}
+                                            {it.name}
                                         </NavLink>
                                     </li>
                                 ))}
