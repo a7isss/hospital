5c19a730430a614b03c0279141b1fe16db3b8e02
A7isss <10.younis@gmail.com>
2025-04-03 09:48:36 +0300

    login fix 11

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,23 +1,12 @@
 import express from 'express';
-import { loginUser, registerUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay, paymentStripe, verifyStripe } from '../controllers/userController.js';
+import { loginUser, registerUser, getProfile, updateProfile } from '../controllers/userController.js';
 import upload from '../middleware/multer.js';
 import authUser from '../middleware/authUser.js';
-import { getServices } from "../controllers/userController.js";
-import { getServiceById } from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
-userRouter.get("/uservices/:id", getServiceById); // New route to fetch service by ID
 userRouter.post("/login", loginUser)
-userRouter.get("/uservices", getServices); // Define the /uservices route
 userRouter.get("/get-profile", authUser, getProfile)
 userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile)
-userRouter.post("/book-appointment", authUser, bookAppointment)
-userRouter.get("/appointments", authUser, listAppointment)
-userRouter.post("/cancel-appointment", authUser, cancelAppointment)
-userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
-userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
-userRouter.post("/payment-stripe", authUser, paymentStripe)
-userRouter.post("/verifyStripe", authUser, verifyStripe)
 
 export default userRouter;
